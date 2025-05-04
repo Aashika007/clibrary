a5cda2021dcf084de382575aac82528fc9cf37c7
Aashika007 <silwalaashika9@gmail.com>
2025-05-04 21:53:49 +0545

    Updated by Aashika Silwal om 4th May 9:53 PM

---
+++ a.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+	int firstnumber,secondnumber,result; 
+	printf("Enter first number:");
+	printf("\n"); 
+	scanf("%d",&firstnumber);
+		printf("\n");
+		
+	printf("Enter second number:");
+	printf("\n"); 
+	scanf("%d",&secondnumber);
+	printf("\n");
+	
+
+	
+	result = firstnumber + secondnumber;
+	
+	
+	printf("Addition = %d",result);
+ 
+	
+}
---
+++ u.c
@@ -0,0 +1,23 @@
+#include<stdio.h> // header file
+int main()
+{
+	int firstnumber,secondnumber,result; //variable initialization
+	printf("Enter first number:");
+	printf("\n"); // /n help to breat line
+	scanf("%d",&firstnumber);
+		printf("\n");
+		
+	printf("Enter second number:");
+	printf("\n"); // /n help to breat line
+	scanf("%d",&secondnumber);
+	printf("\n");
+	
+	//operation or equation
+	
+	result = firstnumber + secondnumber;
+	
+	// display output
+	printf("Addition = %d",result);
+	return 0; //sucesfully run the program
+	
+}
